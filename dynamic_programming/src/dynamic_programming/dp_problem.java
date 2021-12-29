e77c6e2919dddba7e9f04e3a4fec8caaef71da18
schwinnf <franz.schwinn@gmail.com>
2021-12-29 19:50:32 +0300

    j

--- dynamic_programming/src/dynamic_programming/dp_problem.java
+++ dynamic_programming/src/dynamic_programming/dp_problem.java
@@ -1,6 +1,9 @@
 package dynamic_programming;
 
+import java.util.Arrays;
+
 public class dp_problem {
+	// returns random string
 	static String getstring(int length) {
 		String result = "";
 		String alphabet = "ABCDEFG";
@@ -9,5 +12,21 @@ public class dp_problem {
 		}
 		System.out.println("String " + result + " was generated.");
 		return result;
+	}
+	
+	// returns random int array with unique elements
+	static int[] getintegers(int length) {
+		int res[] = new int[length];
+		for(int i = 0; i < res.length; i++) {
+			res[i] = i;
+		}
+		for(int i = 0; i < res.length; i++) {
+			int storage = res[i];
+			int random = (int) (Math.random()*length);
+			res[i] = res[random];
+			res[random] = storage;
+		}
+		System.out.println("Generated array: " + Arrays.toString(res));
+		return res;
 	}
 }
---
+++ dynamic_programming/src/dynamic_programming/longest_increasing_subsequence.java
@@ -0,0 +1,11 @@
+package dynamic_programming;
+
+import java.util.Arrays;
+
+public class longest_increasing_subsequence extends dp_problem {
+	public static void main(String[] args) {
+		int[] integers = getintegers(10);
+		Arrays.sort(integers);
+		
+	}
+}
